45a462fc30230046f4ae10a36bea169937b0f5fb
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-01-29 13:39:22 +0000

    fix a name in bibliografía

--- rubynotes.rb
+++ rubynotes.rb
@@ -1,6 +1,37 @@
 # coding: utf-8
 
 
+=begin
+Seguramente hace falta pa algo las siguientes cosas:
+- clone y dup: hacen una copia superficial del objeto
+
+  + DUP: Produces a shallow copy of obj—the instance variables of obj are copied,
+    but not the objects they reference. *dup copies the tainted state of obj*.
+    This method may have class-specific behavior. If so, that behavior will
+    be documented under the #initialize_copy method of the class.
+
+on dup vs clone
+
+In general, clone and dup may have different semantics in descendant classes.
+While clone is used to duplicate an object, including its internal state,
+dup typically uses the class of the descendant object to create the new instance.
+
+   + CLONE: Produces a shallow copy of obj—the instance variables of obj are copied, but not
+     the objects they reference. clone copies the frozen and tainted state of obj.
+
+
+clone: copia  el estado del objeto → frozen? y tain
+freeze: congela el objeto no se puede modificar
+taint: Es un estado de manchado. Sirve para  confirmar un posible codigo malisioso
+dup: solo copia el estado de manchado
+
+Se define initialize_copy(*origin)
+para generar una copia del objeto
+
+marshal_dump y marshal_load permiten serializar el objeto y desserializarlo
+
+=end
+
 class Ejemplo
   private_class_method :new
 
@@ -70,3 +101,17 @@ m = StoryFlow.new do
   0 => :fin
 end
 
+
+class App
+  private
+  def method
+    puts "hola"
+  end
+  # private :method # ponerlo antes del metodo no sirve
+end
+
+class Game < App
+  def initialize
+    self.method
+  end
+end
